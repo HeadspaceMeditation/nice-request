66cf94831cb435cb0dfecc48801ad1f481b18908
Phu Huynh <phu@headspace.com>
2018-06-14 13:07:15 -0700

    Remove extra test

--- make-request/test/get-request-options.test.js
+++ make-request/test/get-request-options.test.js
@@ -88,23 +88,6 @@ describe('nice-request/make-request/get-request-options', function() {
     });
   });
 
-  it('sets formData when available', function() {
-    expect(getRequestOptions(Object.assign(optionsStub, {
-      formData: 'someFormData',
-      headers: 'headers'
-    }))).to.deep.equal({
-      url: 'https://www.my-nice-test.com/unit-testing-rocks',
-      method: 'GET',
-      json: true,
-      timeout: 1000,
-      metricTag: 'test_request',
-      formData: 'someFormData',
-      headers: 'headers',
-      resolveWithFullResponse: true,
-      rejectUnauthorized: true
-    });
-  });
-
   it('sets the encoding', function() {
     expect(getRequestOptions(Object.assign(optionsStub, {
       encoding: null,
